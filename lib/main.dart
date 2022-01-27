4f6a9531357545709b561dc5150bb4412f273032
Sumin, Vladyslav <vladyslav.sumin@medtronic.com>
2022-01-27 15:27:40 +0200

    initial commit
    initial commit
    
    Added new project structure.

---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+import 'screens/password_screen.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const MyApp());
+}
