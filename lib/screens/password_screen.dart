bd1f0f435746f93ceecd4a472b17ad80b1ef9fb3
Sumin, Vladyslav <vladyslav.sumin@medtronic.com>
2022-01-27 15:27:40 +0200

    initial commit
    initial commit
    
    Added new project structure.

---
+++ lib/screens/password_screen.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Password Generatorr',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('text'),
+      ),
+      body: null,
+    );
+  }
+}
